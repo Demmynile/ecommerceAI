d7f0f388a4c4c7a85ee54bcd50172de0a6866093
demmynile <balogun80@gmail.com>
2026-01-18 16:11:18 +0000

    trying to fix the admin route

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
 const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",
   "/checkout",
-  "/orders",
-  "/orders/[id]",
+  "/orders(.*)",
   "/checkout/success",
 ]);
 
